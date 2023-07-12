a3e42b9432fcd5af8a3bf7eb20207ff5875630ac
EL BACHIR MOURABIT <bachir.mourabit.pro@gmail.com>
2023-07-12 10:11:34 +0200

    Access data resources update

--- moviesRatingsBatch/src/main/scala/com/company/models/MovieModel.scala
+++ moviesRatingsBatch/src/main/scala/com/company/models/MovieModel.scala
@@ -2,8 +2,10 @@ package com.company.models
 
 import org.apache.spark.sql.types.{IntegerType, StringType, StructType}
 
+import java.nio.file.Paths
+
 object MovieModel extends Model {
-  val path: String = "C:\\Users\\bachi\\Spark scala\\workspace\\my-spark-project\\moviesRatingsBatch\\src\\main\\resources\\u.item"
+  val path: String = Paths.get(getClass.getClassLoader.getResource("u.item").toURI).toString
   val delimiter: String = "|"
   val schema: StructType = new StructType()
     .add("movieID", IntegerType)
--- moviesRatingsBatch/src/main/scala/com/company/models/RatingModel.scala
+++ moviesRatingsBatch/src/main/scala/com/company/models/RatingModel.scala
@@ -1,9 +1,12 @@
 package com.company.models
 
+import com.company.models.MovieModel.getClass
 import org.apache.spark.sql.types.{IntegerType, StructType}
 
+import java.nio.file.Paths
+
 object RatingModel extends Model {
-  val path: String = "C:\\Users\\bachi\\Spark scala\\workspace\\my-spark-project\\moviesRatingsBatch\\src\\main\\resources\\u.data"
+  val path: String = Paths.get(getClass.getClassLoader.getResource("u.data").toURI).toString
   val delimiter: String = "\t"
   val schema: StructType = new StructType()
     .add("userID", IntegerType)
